49625bb63168acb2976e459d581d9eec009ff84a
Koko <78096367+UselessNode@users.noreply.github.com>
2022-11-23 13:00:08 +0700

    Добавьте файлы проекта.

---
+++ gChart/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace gChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.bExecute = new System.Windows.Forms.Button();
+            this.bRandom = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.tBMaxMin = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tBMinMax = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(712, 588);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.bExecute);
+            this.tabPage1.Controls.Add(this.bRandom);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(704, 557);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Таблица";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // bExecute
+            // 
+            this.bExecute.AutoSize = true;
+            this.bExecute.Location = new System.Drawing.Point(3, 214);
+            this.bExecute.Name = "bExecute";
+            this.bExecute.Size = new System.Drawing.Size(236, 28);
+            this.bExecute.TabIndex = 2;
+            this.bExecute.Text = "Решить";
+            this.bExecute.UseVisualStyleBackColor = true;
+            this.bExecute.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bRandom
+            // 
+            this.bRandom.Location = new System.Drawing.Point(3, 158);
+            this.bRandom.Name = "bRandom";
+            this.bRandom.Size = new System.Drawing.Size(234, 50);
+            this.bRandom.TabIndex = 0;
+            this.bRandom.Text = "Заполнить случайными числами";
+            this.bRandom.UseVisualStyleBackColor = true;
+            this.bRandom.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.flowLayoutPanel3);
+            this.groupBox2.Controls.Add(this.flowLayoutPanel4);
+            this.groupBox2.Location = new System.Drawing.Point(-2, 248);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 124);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вывод";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.Controls.Add(this.Label4);
+            this.flowLayoutPanel3.Controls.Add(this.tBMaxMin);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(6, 65);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(227, 33);
+            this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // Label4
+            // 
+            this.Label4.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(3, 0);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(68, 18);
+            this.Label4.TabIndex = 0;
+            this.Label4.Text = "MaxMin";
+            this.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tBMaxMin
+            // 
+            this.tBMaxMin.Location = new System.Drawing.Point(77, 3);
+            this.tBMaxMin.Name = "tBMaxMin";
+            this.tBMaxMin.ReadOnly = true;
+            this.tBMaxMin.Size = new System.Drawing.Size(147, 27);
+            this.tBMaxMin.TabIndex = 1;
+            this.tBMaxMin.TabStop = false;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.AutoSize = true;
+            this.flowLayoutPanel4.Controls.Add(this.label3);
+            this.flowLayoutPanel4.Controls.Add(this.tBMinMax);
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(6, 26);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(227, 33);
+            this.flowLayoutPanel4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "MinMax";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tBMinMax
+            // 
+            this.tBMinMax.Location = new System.Drawing.Point(77, 3);
+            this.tBMinMax.Name = "tBMinMax";
+            this.tBMinMax.ReadOnly = true;
+            this.tBMinMax.Size = new System.Drawing.Size(147, 27);
+            this.tBMinMax.TabIndex = 1;
+            this.tBMinMax.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 117);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Количество решений";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 26);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(76, 27);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(704, 557);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Графики";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 27);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1080, 557);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Справка";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(302, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(399, 551);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 59);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(76, 27);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 588);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MinimumSize = new System.Drawing.Size(299, 200);
+            this.Name = "Form1";
+            this.Text = "gChart";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.TextBox tBMaxMin;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tBMinMax;
+        private System.Windows.Forms.Button bRandom;
+        private System.Windows.Forms.Button bExecute;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ gChart/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace gChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTable();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            LoadTable();
+        }
+
+        void LoadTable()
+        {
+            dataGridView1.ColumnCount = (int)numericUpDown1.Value;
+            dataGridView1.RowCount =    (int)numericUpDown2.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int rCount = dataGridView1.RowCount;
+            int cCount = dataGridView1.ColumnCount;
+            Text = $"gChart | {rCount} x {cCount}";
+            for (int r = 0; r < rCount; r++)
+            {
+                for (int c = 0; c < cCount; c++)
+                {
+                    dataGridView1.Rows[r].Cells[c].Value = rand.Next(10);
+                    //dataGridView1.Rows[r].Cells[c].Value = $"{r}:{c}";
+                }
+            }
+            
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //GetResultCalc();
+        }
+
+        void GetResultCalc()
+        {
+            int[,] data = GetArrayFromDataGridView();
+            int rowSize = data.GetUpperBound(0);
+            int colSize = data.GetUpperBound(1);
+            List<List<int>> cols = new List<List<int>>(colSize);
+            List<List<int>> rows = new List<List<int>>(rowSize);
+            List<int> maxList = new List<int>();
+            List<int> minList = new List<int>();
+
+            // 100% Работает для 2-ух строк
+            //List<int> row1 = new List<int>();
+            //List<int> row2 = new List<int>();
+
+            //// 100% Работает для 2-ух строк
+            //for (int i = 0; i < colSize; i++)
+            //{
+            //    row1.Add(data[0, i]);
+            //    row2.Add(data[1, i]);
+            //}
+
+            // 100% работает для 2-ух строк
+            //int minRow1 = row1.Min();
+            //int minRow2 = row2.Min();
+            //int minmax = Math.Max(minRow1, minRow2);
+            //
+
+            Minimax(data, rowSize, colSize, rows, minList);
+
+            Maximin(data, rowSize, colSize, cols, maxList);
+        }
+
+        private void Minimax(int[,] data, int rowSize, int colSize, List<List<int>> rows, List<int> minList)
+        {
+            // Запись значений строк в массив строк
+            for (int row = 0; row < rowSize + 1; row++)
+            {
+                List<int> temp = new List<int>();
+                for (int col = 0; col < colSize + 1; col++)
+                {
+                    temp.Add(data[row, col]);
+                }
+                rows.Add(temp);
+            }
+
+            foreach (List<int> list in rows)
+            {
+                minList.Add(list.Min());
+            }
+            int minmax = minList.Max();
+            tBMinMax.Text = minmax.ToString();
+        }
+
+        private void Maximin(int[,] data, int rowSize, int colSize, List<List<int>> cols, List<int> maxList)
+        {
+            // Запись значений столбцов в массив столбцов
+            for (int col = 0; col < colSize; col++)
+            {
+                List<int> temp = new List<int>();
+                for (int row = 0; row < rowSize + 1; row++)
+                {
+                    temp.Add(data[row, col]);
+                }
+                cols.Add(temp);
+            }
+
+            foreach (List<int> list in cols)
+            {
+                maxList.Add(list.Max());
+            }
+            int maxmin = maxList.Min();
+            tBMaxMin.Text = maxmin.ToString();
+        }
+
+        private int[,] GetArrayFromDataGridView()
+        {
+            int rCount = dataGridView1.RowCount;
+            int cCount = dataGridView1.ColumnCount;
+            int[,] data = new int[rCount, cCount];
+            for (int r = 0; r < rCount; r++)
+            {
+                for (int c = 0; c < cCount; c++)
+                {
+                    //dataGridView1.Rows[r].Cells[c].Value;
+                    var x = dataGridView1.Rows[r].Cells[c].Value;
+                    data[r, c] = Convert.ToInt16(x);
+                }
+            }
+            return data;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetResultCalc();
+        }
+
+
+        // Тестовые кнопки (потом удалить)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Строки
+
+            int rCount = dataGridView1.RowCount;    // Строка
+            int cCount = dataGridView1.ColumnCount; // Столбец
+
+
+            for (int i = 0; i < rCount; i++)        // Строка
+            {
+                for (int j = 0; j < cCount; j++)
+                {
+                    dataGridView1.Rows[0].Cells[j].Style.BackColor = Color.Turquoise;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Столбцы
+
+            int rCount = dataGridView1.RowCount;    // Строка
+            int cCount = dataGridView1.ColumnCount; // Столбец
+
+            for (int i = 0; i < cCount; i++)        // Столбец
+            {
+                
+                for (int j = 0; j < rCount; j++)    //
+                {
+                    dataGridView1.Rows[i].Cells[0].Style.BackColor = Color.Tomato;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int rowNumber = 1;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+                row.HeaderCell.Value = "Row " + rowNumber;
+                rowNumber = rowNumber + 1;
+            }
+            dataGridView1.AutoResizeRowHeadersWidth(
+                DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+        }
+    }
+}
+
